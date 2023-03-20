4c2a3fba62890beff203fe9df902bd3d988de71e
Chelsea <113374977+ChelseaLofton@users.noreply.github.com>
2023-03-20 14:57:24 -0700

    plant buttons working

--- static/jsx/app.jsx
+++ static/jsx/app.jsx
@@ -4,6 +4,8 @@ function App () {
         <div>
 
             <GenerateHumidityButtons />
+            <PlantButtons />
+            <GenerateSensorButtons  />
         </div>
     );
 };
--- static/jsx/plants.jsx
+++ static/jsx/plants.jsx
@@ -1,7 +1,7 @@
-// import App from './app.jsx';
-
 const PlantButtons = () => {
     const [plantIds, setPlantIds] = React.useState([]);
+    const [sensorData, setSensorData] = React.useState(null);
+    const [plantBookData, setPlantBookData] = React.useState(null);
 
     React.useEffect(() => {
         fetch('/plants.json')
@@ -16,54 +16,57 @@ const PlantButtons = () => {
             .then(response => response.json())
             .then(data => {
                 const sensor_readings = data.sensor_readings;
-                const sensor_battery = sensor_readings.battery;
-                const sensor_conductivity = sensor_readings.conductivity;
-                const sensor_illuminance = sensor_readings.illuminance;
-                const sensor_moisture = sensor_readings.moisture;
-                const sensor_temperature = sensor_readings.temperature;
-
                 const plant_data = data.plant_data;
 
-                const pid = plant_data.pid;
-                const alias = plant_data.alias;
-                const category = plant_data.category;
-                const max_light_lux = plant_data.max_light_lux;
-                const min_light_lux = plant_data.min_light_lux;
-                const max_temp = plant_data.max_temp;
-                const min_temp = plant_data.min_temp;
-                const max_env_humid = plant_data.max_env_humid;
-                const min_env_humid = plant_data.min_env_humid;
-                const max_soil_moist = plant_data.max_soil_moist;
-                const min_soil_moist = plant_data.min_soil_moist;
-                const max_soil_ec = plant_data.max_soil_ec;
-                const min_soil_ec = plant_data.min_soil_ec;
-                const image_url = plant_data.image_url;
-
-                document.querySelector('#plant-sensor-data').innerHTML = `Battery:${sensor_battery}%, Conductivity:${sensor_conductivity}, Illuminance:${sensor_illuminance}lux, Soil Moisture:${sensor_moisture}%, Temperature:${sensor_temperature}°F`;
-                document.querySelector('#plantbook-data').innerHTML = `PID:${pid}, Alias:${alias}, Category:${category}, Max Light:${max_light_lux}, Min Light:${min_light_lux}, Max Temp:${max_temp}°F, Min Temp:${min_temp}°F, Max Environment Humidity:${max_env_humid}%, Min Environment Humidity:${min_env_humid}%, Max Soil Moisture:${max_soil_moist}%, Min Soil Moisture:${min_soil_moist}%, Max Soil EC:${max_soil_ec}, Min Soil EC:${min_soil_ec}, Image URL:${image_url}`;
+                setSensorData(sensor_readings)
+                setPlantBookData(plant_data)
             });
     };
 
     return (
         <React.Fragment>
             <h2>All Plant Data</h2>
-        <div id="plant-ids">
-            {plantIds.map((plantID) => (
-                <button
-                    key={`sensor-${plantID}`}
-                    id={`sensors=${plantID}-button`}
-                    onClick={() => handleButtonClick(plantID)}
-                >
-                    {plantID}
-                </button>
-                
-            ))}
-        </div>
+            <div id="plant-ids">
+                {plantIds.map((plantID) => (
+                    <div>
+                        <button
+                            key={`sensor-${plantID}`}
+                            id={`sensors=${plantID}-button`}
+                            onClick={() => handleButtonClick(plantID)}
+                        >
+                            {plantID}
+                        </button>
+                    </div>
+                ))}
+            </div>
+            <div>
+                <h2> Current Sensor Readings</h2>
+                {sensorData && <div id="plant-sensor-data">
+                    Battery: {sensorData.battery};
+                    Conductivity: {sensorData.conductivity};
+                    Illuminance: {sensorData.illuminance};
+                    Moisture: {sensorData.moisture};
+                    Temperature: {sensorData.temperature};
+                </div>}
+            </div>
+            <div>
+                <h2>Plant Book Data</h2>
+                {plantBookData && <div id="plantbook-data">
+                    Alias: {plantBookData.alias};
+                    Category: {plantBookData.category};
+                    Max Light: {plantBookData.max_light_lux};
+                    Min Light: {plantBookData.min_light_lux};
+                    Max Temperature: {plantBookData.max_temp};
+                    Min Temperature: {plantBookData.min_temp};
+                    Max Env. Humidity: {plantBookData.max_env_humid};
+                    Min Env. Humidity: {plantBookData.min_env_humid};
+                    Max Soil Moisture: {plantBookData.max_soil_moist};
+                    Min Soil Moisture = {plantBookData.min_soil_moist};
+                    Max Soil EC: {plantBookData.max_soil_ec};
+                    Min Soil EC: {plantBookData.min_soil_ec};
+                    Image: {plantBookData.image_url};
+                </div>}
+            </div>
         </React.Fragment>
     );
 };
-
-
-// export default PlantButtons;
-
-// ReactDOM.render(<PlantButtons />, document.getElementById('plant-ids'));
