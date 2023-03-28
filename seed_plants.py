05685eb5b2c46890eb89579d3c4790c43a5c3682
Chelsea <113374977+ChelseaLofton@users.noreply.github.com>
2023-03-28 14:53:49 -0700

    seed_plants organized and docstrings added

--- seed_plants.py
+++ seed_plants.py
@@ -1,31 +1,39 @@
-import model
-import crud 
-import server
-import json
+"""
+This script creates instances of the Plant model and populates the database with the plant data.
+It must be run after the plant book is seeded.
 
+Language: Python
+Frameworks/Libraries: Flask-SQLAlchemy
+Database: SQLAlchemy
+"""
+
+import json
 from flask_sqlalchemy import SQLAlchemy
 
+import model
+import crud
+import server
 
-
+# Connect to the database and create all tables
 model.connect_to_db(server.app)
 model.db.create_all()
 
-
-"""Run after running seed-databases.py"""
-
-
+# Load plant data from JSON file
 with open("data/plant_data.json") as f:
     plant_data = json.loads(f.read())
 
-for plant_id, plant_values in plant_data.items():               #must be run after plant book is seeded
+# Iterate through plant data and create Plant instances
+for plant_id, plant_values in plant_data.items():
+    
     plant_values['sensor_id'] = plant_id.split(" ")[-1]
 
-
+    # Extract pid and sensor_id
     pid, sensor_id = (
         plant_values['pid'],
         plant_values['sensor_id']
     )
 
+    # Create a new Plant instance and add it to the database
     new_plant = crud.create_plant(plant_id, pid, sensor_id)
 
     model.db.session.merge(new_plant)
