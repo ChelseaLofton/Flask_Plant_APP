a6b9ddeee6b48a961b3d704c313c2a97f4c843f9
Chelsea <113374977+ChelseaLofton@users.noreply.github.com>
2023-05-10 12:50:16 -0700

    removed button displays most recent values on load

--- static/jsx/humidity.jsx
+++ static/jsx/humidity.jsx
@@ -11,57 +11,57 @@
 
 
 // HumidityModal component displays live sensor readings in a modal
-const HumidityModal = (props) => {
-    const { humidityData, showModal, selectedHumidityId, handleClose } = props;
-    const displayName = getNameForId(selectedHumidityId);
+// const HumidityModal = (props) => {
+//     const { humidityData, showModal, selectedHumidityId, handleClose } = props;
+//     const displayName = getNameForId(selectedHumidityId);
 
-    return (
-        <div
-            className={`modal fade ${showModal ? "show" : ""}`}
-            tabIndex="-1"
-            aria-hidden={!showModal}
-            style={{ display: showModal ? "block" : "none" }}
-        >
-            <div class="modal-dialog modal-sm">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h2 className="modal-title">{displayName} Atmospheric Humidity</h2>
+//     return (
+//         <div
+//             className={`modal fade ${showModal ? "show" : ""}`}
+//             tabIndex="-1"
+//             aria-hidden={!showModal}
+//             style={{ display: showModal ? "block" : "none" }}
+//         >
+//             <div class="modal-dialog modal-sm">
+//                 <div className="modal-content">
+//                     <div className="modal-header">
+//                         <h2 className="modal-title">{displayName} Atmospheric Humidity</h2>
 
-                        <button type="button" className="btn-close" onClick={handleClose}></button>
-                    </div>
-                    <div className="modal-body">
-                        {humidityData && (
-                            <div id="plant-sensor-data">
-                                <ul>
-                                    <li>Humidity: {humidityData.humidity}%</li>
-                                    <li>Pressure: {humidityData.pressure}</li>
-                                    <li>Temperature: {humidityData.temperature}°F</li>
-                                    <li>Battery: {humidityData.battery}%</li>
-                                </ul>
-                            </div>
-                        )}
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-};
+//                         <button type="button" className="btn-close" onClick={handleClose}></button>
+//                     </div>
+//                     <div className="modal-body">
+//                         {humidityData && (
+//                             <div id="plant-sensor-data">
+//                                 <ul>
+//                                     <li>Humidity: {humidityData.humidity}%</li>
+//                                     <li>Pressure: {humidityData.pressure}</li>
+//                                     <li>Temperature: {humidityData.temperature}°F</li>
+//                                     <li>Battery: {humidityData.battery}%</li>
+//                                 </ul>
+//                             </div>
+//                         )}
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
 
 
 // HumidityButton component renders individual humidity sensor buttons
-const HumidityButton = (props) => {
-    const displayName = getNameForId(props.id);
-    return (
-        <button
-            key={`humidity-${props.id}`}
-            id={`sensor=${props.id}-button`}
-            className="humidity-button"
-            onClick={() => props.onClick(props.id)}
-        >
-            {displayName}
-        </button>
-    );
-};
+// const HumidityButton = (props) => {
+//     const displayName = getNameForId(props.id);
+//     return (
+//         <button
+//             key={`humidity-${props.id}`}
+//             id={`sensor=${props.id}-button`}
+//             className="humidity-button"
+//             onClick={() => props.onClick(props.id)}
+//         >
+//             {displayName}
+//         </button>
+//     );
+// };
 
 function getNameForId(id) {
     switch (id) {
@@ -78,55 +78,69 @@ function getNameForId(id) {
 
 function Humidity() {
     const [humidityIds, setHumidityIds] = React.useState([]);
-    const [humidityData, setHumidityData] = React.useState(null);
+    const [humidityData, setHumidityData] = React.useState({});
     const [showModal, setShowModal] = React.useState(false);
     const [selectedHumidityId, setSelectedHumidityId] = React.useState(null);
 
-    // Fetch humidity sensor IDs from API
+    // Fetch humidity sensor IDs and data from API
     React.useEffect(() => {
         const url = "/humidity.json";
         fetch(url)
             .then((response) => response.json())
             .then((data) => {
                 setHumidityIds(data);
+                const humidityDataPromises = data.map((humidityId) => {
+                    const humidityDataUrl = `/humidity/${humidityId}.json`;
+                    return fetch(humidityDataUrl)
+                        .then((response) => response.json())
+                        .then((data) => ({ [humidityId]: data }));
+                });
+                Promise.all(humidityDataPromises).then((sensorData) => {
+                    const mergedSensorData = Object.assign({}, ...sensorData);
+                    setHumidityData(mergedSensorData);
+                    setSelectedHumidityId(data[0]);
+                });
             });
     }, []);
 
     // Handle click event on a humidity sensor button
-    const handleHumidityClick = (humidityId) => {
-        const url = `/humidity/${humidityId}.json`;
-        // console.log(humidityId);
-        fetch(url)
-            .then((response) => response.json())
-            .then((data) => {
-                setHumidityData(data);
-                setSelectedHumidityId(humidityId);
-                setShowModal(true);
-            });
-    };
+    // const handleHumidityClick = (humidityId) => {
+    //     const url = `/humidity/${humidityId}.json`;
+    //     // console.log(humidityId);
+    //     fetch(url)
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             setHumidityData(data);
+    //             setSelectedHumidityId(humidityId);
+    //             setShowModal(true);
+    //         });
+    // };
 
-    // Handle closing the modal 
-    const handleClose = () => {
-        setShowModal(false);
-    };
+    // // Handle closing the modal 
+    // const handleClose = () => {
+    //     setShowModal(false);
+    // };
 
     return (
         <React.Fragment>
-            <div id="humidity-ids">
-                {humidityIds.map((humidityID) => (
-                    <div key={humidityID} className="humidity-item">
-                        <HumidityButton id={humidityID} onClick={handleHumidityClick} />
-                    </div>
-                ))}
-            </div>
-            <div>
-                <HumidityModal
-                    humidityData={humidityData}
-                    selectedHumidityId={selectedHumidityId}
-                    showModal={showModal}
-                    handleClose={handleClose}
-                />
-            </div>
+            {humidityIds.map((humidityId) => (
+                <div key={humidityId} className="humidity-item">
+                    <h2>{getNameForId(humidityId)} Atmospheric Humidity</h2>
+                    {humidityData[humidityId] && (
+                        <ul>
+                            <li>Humidity: {humidityData[humidityId].humidity}%</li>
+                            <li>Pressure: {humidityData[humidityId].pressure}</li>
+                            <li>Temperature: {humidityData[humidityId].temperature}°F</li>
+                            <li>Battery: {humidityData[humidityId].battery}%</li>
+                        </ul>
+                    )}
+                </div>
+            ))}
         </React.Fragment>
     );
 }
+
+
+
+
+
